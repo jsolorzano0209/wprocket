ce0a15ee62421e77ad3f7687b9daabd236771c93
jsolorzano0209 <jsolorzano0209@gmail.com>
2021-02-22 09:19:41 -0500

    Update hideadminnotices-wprocket-notices.php

--- hideadminnotices-wprocket-notices.php
+++ hideadminnotices-wprocket-notices.php
@@ -1,9 +1,12 @@
 <?php
 
+/* Not fully tested */
+
 add_action('in_admin_header', function () {
  {
   remove_action('admin_notices','rocket_warning_htaccess_permissions');
   remove_action('admin_notices','rocket_warning_cache_dir_permissions');
+
   
   }
   
